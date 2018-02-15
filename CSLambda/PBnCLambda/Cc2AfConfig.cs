43c6fe08b908545008f7c03a79a007d19ae90ea8
Mark Kraus <github@markekraus.com>
2018-02-15 14:19:04 -0600

    WIP deployment trigger

---
+++ CSLambda/PBnCLambda/Cc2AfConfig.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PBnCLambda
+{
+    public class Cc2AfConfig
+    {
+        public string DeploymentUser;
+        public string DeploymentPassword;
+        public string DeploymentTriggerUrl;
+        public string DeploymentAppName;
+
+        public string CodeCommitUser;
+        public string CodeCommitPassword;
+    }
+}
--- CSLambda/PBnCLambda/Function.cs
+++ CSLambda/PBnCLambda/Function.cs
@@ -1,14 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 
+using Amazon.KeyManagementService;
+using Amazon.KeyManagementService.Model;
 using Amazon.Lambda.Core;
 using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
 using Amazon.CodeCommit;
 using Amazon.CodeCommit.Model;
 using Newtonsoft.Json;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using System.Net.Http;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -18,6 +25,11 @@ namespace PBnCLambda
     public class Function
     {
 
+        private static string ConfigPathEnvVar = "CC2AF_CONFIG_PATH";
+        private static string ConfigPathDefault = "cc2af.yaml";
+        
+        private static HttpClient Http = new HttpClient();
+        
         /// <summary>
         /// A function to handle a CodeCommit Event.
         /// </summary>
@@ -26,22 +38,73 @@ namespace PBnCLambda
         /// <returns></returns>
         public void FunctionHandler(CodeCommitEvent commitEvent, ILambdaContext context)
         {
-            string repoName = commitEvent.Records[0].RepositoryName;
+            string repositoryName = commitEvent.Records[0].RepositoryName;
             string branch = commitEvent.Records[0].codecommit.references[0].Branch;
             string commit = commitEvent.Records[0].codecommit.references[0].commit;
+
+            string configPath = Environment.GetEnvironmentVariable(ConfigPathEnvVar);
+            configPath = String.IsNullOrEmpty(configPath) ? ConfigPathDefault : configPath;
+
             var codeCommit = new AmazonCodeCommitClient();
-            var repo = codeCommit.GetRepositoryAsync( new GetRepositoryRequest() {RepositoryName = repoName}).GetAwaiter().GetResult();
-            var differences = GetDifferences(codeCommit, repoName, commit);
+            var repository = codeCommit.GetRepositoryAsync( 
+                new GetRepositoryRequest() {RepositoryName = repositoryName}).GetAwaiter().GetResult();
+            
+            var config = GetCc2AfConfig(codeCommit, repositoryName, commit, configPath);
+
+            var deploymentPassword = ConvertFromEncryptedBase64(config.DeploymentPassword);
+            var codeCommitPassword = ConvertFromEncryptedBase64(config.CodeCommitPassword);
+
+
 
             var sns = new AmazonSimpleNotificationServiceClient();
             var publishRequest = new PublishRequest();
-            publishRequest.Message = $"Repo: {commitEvent.Records[0].RepositoryName}; CommitId: {commitEvent.Records[0].codecommit.references[0].commit}; Ref: {commitEvent.Records[0].codecommit.references[0].@ref}";
+            publishRequest.Message = $"Repository: {repositoryName}; CommitId: {commit}; Branch: {branch}";
             publishRequest.Subject = "Test SNS";
             publishRequest.TopicArn = "arn:aws:sns:us-east-1:099223339714:TestTrigger";
             var result = sns.PublishAsync(publishRequest).GetAwaiter().GetResult();
             Console.WriteLine(JsonConvert.SerializeObject(result));
         }
 
+        private string ConvertFromEncryptedBase64 (string encryptedBase64)
+        {
+            string result;
+            using (var kms = new AmazonKeyManagementServiceClient())
+            {
+                var response = kms.DecryptAsync(new DecryptRequest(){
+                    CiphertextBlob = new MemoryStream(Convert.FromBase64String(encryptedBase64))
+                }).GetAwaiter().GetResult();
+                using (TextReader reader = new StreamReader(response.Plaintext))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+            return result;
+        }
+
+        private Cc2AfConfig GetCc2AfConfig(AmazonCodeCommitClient codeCommit, string repositoryName, string afterCommitSpecifier, string configPath)
+        {
+            Cc2AfConfig result;
+            var differences = GetDifferences(codeCommit, repositoryName, afterCommitSpecifier);
+
+            var configDiff = GetConfigurationDifference(differences, configPath);
+            if (configDiff == null)
+            {
+                var ex = new FileNotFoundException("Unable to find Cc2Fa Configuration YAML file.", configPath);
+                throw ex;
+            }
+
+            var configBlob = codeCommit.GetBlobAsync(new GetBlobRequest(){
+                BlobId = configDiff.AfterBlob.BlobId,
+                RepositoryName = repositoryName
+            }).GetAwaiter().GetResult();
+
+            var yamlDeserializer = new DeserializerBuilder().WithNamingConvention(new CamelCaseNamingConvention()).Build();
+            using (TextReader reader = new StreamReader(configBlob.Content))
+            {
+                result = yamlDeserializer.Deserialize<Cc2AfConfig>(reader);
+            }
+            return result;
+        }
         private List<Difference> GetDifferences (AmazonCodeCommitClient codeCommit, string repositoryName, string afterCommitSpecifier)
         {
             var result = new List<Difference>();
@@ -60,6 +123,27 @@ namespace PBnCLambda
             } while (!String.IsNullOrEmpty(nextToken));
 
             return result;
+        }
+
+        private Difference GetConfigurationDifference(IList<Difference> differences, string configPath)
+        {
+            Difference result;
+            result = differences.Where( d => d.AfterBlob.Path == configPath).FirstOrDefault();
+            return result;
+        }
+
+        private void InvokeDeployment(HttpClient http, string deploymentAppUrl, string deploymentUser, string deploymentPassword, string deploymentAppName, string codeCommitUser, string codeCommitPassword, string codeCommitHttpsUrl)
+        {
+            var auth = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{deploymentUser}:{deploymentPassword}"));
+            var request = new HttpRequestMessage();
+            request.Headers.Add("Authorization", $"Basic {auth}");
+            request.Headers.Add("X-SITE-DEPLOYMENT-ID",deploymentAppName);
+            request.RequestUri = new Uri(deploymentAppUrl);
+
+            var builder = new UriBuilder(codeCommitHttpsUrl);
+            builder.UserName = codeCommitUser;
+            builder.Password = deploymentPassword;
+            
         }
     }
 }

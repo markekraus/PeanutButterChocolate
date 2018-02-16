2127dab1adc4cbb6dbe51801e5a3771058c4cc8b
Mark Kraus <github@markekraus.com>
2018-02-16 08:30:35 -0600

    WIP reworking configuration.ps1

--- Configuration.ps1
+++ Configuration.ps1
@@ -15,36 +15,40 @@ if ($PSScriptRoot) {
     $BaseDir = $PSScriptRoot
 }
 
-# Log in to Azure
-Login-AzureRmAccount
-
 $AWSCreds = Get-Credential -Message 'AWS Access Key and Secret Key'
 # Settlings used by this project
 $Settings = @{
-    SrcDirectory = Join-Path $BaseDir src
+    SrcDirectory                  = Join-Path $BaseDir src
     # Folder under which the Git repo will be cloned 
-    GitDirectory = 'c:\Git'
+    GitDirectory                  = 'c:\Git'
     # Azure ResourceGroup Name
     ResourceGroupName             = 'PBnC'
     # See the -Location parameter of New-AzureRmResourceGroup for details
     ResourceGroupLocation         = "South Central US"
     # Options: Standard_LRS, Standard_GRS, Standard_RAGRS
     FunctionAppStorageAccountType = 'Standard_LRS'
-    AwsAccessKey      = $AWSCreds.UserName
-    AWSSecretKey      = $AWSCreds.GetNetworkCredential().Password
-    AwsProfile        = 'Aws01'
-    AwsRegion         = 'us-east-2'
-    CCRepoName        = 'PBnC'
-    CCRepoDescription = 'Peanut Butter and Chocolate'
+    AwsAccessKey                  = $AWSCreds.UserName
+    AWSSecretKey                  = $AWSCreds.GetNetworkCredential().Password
+    AwsProfile                    = 'Aws01'
+    AwsRegion                     = 'us-east-2'
+    CCRepoName                    = 'PBnC'
+    CCRepoDescription             = 'Peanut Butter and Chocolate'
+    CCGitUser                     = 'PBnC-Git-User'
+    CCGitUserPolicyName           = 'PBnC-Git-User-{0}' -f [Guid]::NewGuid()
 }
 
-# Create the Git Directory and change location to it
-$Null = New-Item -ItemType Directory -Path $Settings.GitDirectory -Force -ErrorAction 'SilentlyContinue'
-Push-Location $Settings.GitDirectory
-
-# Hashtables used for storing results
+# Hashtables used for storing results from commands
 $AzureAssets = @{}
 $AWSAssets = @{}
+
+
+# Log in to Azure
+$AzureAssets['AccountLogin'] = Login-AzureRmAccount
+
+# Create AWS credential profile and set it as the default configuration
+Set-AWSCredential -StoreAs $Settings.AwsProfile -AccessKey $Settings.AwsAccessKey -SecretKey $Settings.AWSSecretKey
+Initialize-AWSDefaultConfiguration -ProfileName $Settings.AwsProfile -Region $Settings.AwsRegion 
+
 
 # Create the PBnC Resource Group and add a Function App Deployment
 $Params = @{
@@ -62,18 +66,6 @@ $Params = @{
     storageAccountType = $Settings.FunctionAppStorageAccountType
 }
 $AzureAssets['Deployment'] = New-AzureRmResourceGroupDeployment @Params -ErrorVariable 'DeploymentErrors' 
-
-# Enable Local Git based deployment on the Web App
-$Params = @{
-    PropertyObject    = @{
-        scmType = "LocalGit"
-    }
-    ResourceGroupName = $Settings.ResourceGroupName 
-    ResourceType      = 'Microsoft.Web/sites/config'
-    ResourceName      = '{0}/web' -f $Settings.ResourceGroupName
-    ApiVersion        = '2015-08-01'
-}
-$AzureAssets['WebAppProperties'] = Set-AzureRmResource @Params -Force
 
 # Validate that the deployment was successful
 Describe "Deployment of '$($Settings.ResourceGroupName)' Azure Function App" {
@@ -115,10 +107,6 @@ $AzureAssets['WebAppDeployUrl'] = 'https://{0}:{1}@{2}.scm.azurewebsites.net:443
     $AzureAssets.ResourceGroup.ResourceGroupName
 )
 
-# Create a credential profile
-Set-AWSCredential -StoreAs $Settings.AwsProfile -AccessKey $Settings.AwsAccessKey -SecretKey $Settings.AWSSecretKey
-Initialize-AWSDefaultConfiguration -ProfileName $Settings.AwsProfile -Region $Settings.AwsRegion 
-
 $Params = @{
     RepositoryName        = $Settings.CCRepoName
     RepositoryDescription = $Settings.CCRepoDescription
@@ -134,21 +122,156 @@ Describe "Deployment of '$($Settings.CCRepoName)' AWS CodeCommit Repo" {
     }
 }
 
-# Copy the Sample function app, commit the changes and push to AWS CodeCommit
+# Create a new AWS IAM User required for CodeCommit Git Credentials
+$Params = @{
+    UserName = $Settings.CCGitUser
+    Force = $true
+}
+$AWSAssets['IAMUser'] = New-IAMUser @Params
+
+# Create the IAM policy document and set it for the user
+$PolicyDocument = @"
+{
+  "Version": "2012-10-17",
+  "Statement" : [
+    {
+      "Effect" : "Allow",
+      "Action" : [
+        "codecommit:GitPull"
+      ],
+      "Resource" : [
+        "$($AWSAssets.CCRepo.Arn)"
+      ]
+    }
+  ]
+}
+"@
+$Params = @{
+    PolicyDocument = $PolicyDocument
+    PolicyName = $Settings.CCGitUserPolicyName
+    userName = $AWSAssets.IAMUser.UserName
+    force = $true
+    PassThru = $true
+}
+$AWSAssets['IAMPolicy'] = Write-IAMUserPolicy @Params
+
+Describe "AWS Git User" {
+    It "Was successfully created" {
+        $user = Get-IAMUser -UserName $Settings.CCGitUser
+        $user.UserName | should -BeExactly $Settings.CCGitUser
+    }
+    It "Has a policy attached" {
+        $policy = Get-IAMUserPolicy -PolicyName $Settings.CCGitUserPolicyName -UserName $Settings.CCGitUser
+        $PolicyObj = [uri]::UnescapeDataString($policy.PolicyDocument) | ConvertFrom-Json
+        
+        $PolicyObj.Statement[0].Effect | should -BeExactly 'Allow'
+        $PolicyObj.Statement[0].Action[0] | should -BeExactly 'codecommit:GitPull'
+        $PolicyObj.Statement[0].Resource[0] | should -BeExactly $AWSAssets.CCRepo.Arn
+    }
+}
+
+# At this point we need to use th AWS web console to generate 
+$ConsoleUrl = 'https://console.aws.amazon.com/iam/home?region={0}#/users/{1}section=security_credentials' -f 
+    $Settings.AwsRegion, $Settings.CCGitUser
+Write-Host @"
+
+
+Before continuing, you must generate HTTPS Git credentials for AWS CodeCommit for the $($Settings.CCGitUser) user.
+Currently, this can only be done in the AWS web console.
+You will need to use a web browser to login to the AWS console with an user that has IAM edit permissions.
+Then navigate to 
+
+    $ConsoleUrl
+
+Once you generate the HTTPS Git credentials for AWS CodeCommit, enter the generated username and password.
+
+
+"@
+$AWSAssets['CCGitUserCredentials'] = Get-Credential -Message "HTTPS Git credentials for AWS CodeCommit"
+
+# Create the Git Directory and change location to it
+$Null = New-Item -ItemType Directory -Path $Settings.GitDirectory -Force -ErrorAction 'SilentlyContinue'
+Push-Location $Settings.GitDirectory
+
+# Clone the empty CodeCommit repository
 git clone $AWSAssets.CCRepo.CloneUrlSsh
 Push-Location $Settings.ResourceGroupName
+
+# Add an empty host.json file. This will let us confirm the Azure Web App Deployment settings are working
+'{}' | Set-Content 'host.json'
+git add -A
+git commit -m 'Initial Commit'
+git push --force
+
+Describe "Initial Git Commit" {
+    It "Was successful" {
+        $diffs = Get-CCDifferenceList -AfterCommitSpecifier "master" -RepositoryName $Settings.ResourceGroupName
+        
+        $diffs.count | Should -BeExactly 1
+        $diffs[0].ChangeType | Should -BeExactly 'A'
+        $diffs[0].AfterBlob.Path | Should -BeExactly 'host.json'
+    }
+}
+
+# Build the Git URL to include the Git user credentials
+$builder = [UriBuilder]::new($AWSAssets.CCRepo.CloneUrlHttp)
+$builder.UserName = $AWSAssets.CCGitUserCredentials.UserName
+$builder.Password = $AWSAssets.CCGitUserCredentials.GetNetworkCredential().Password
+
+
+# Configure the deployment settings on the Azure Web App to use the AWS CodeCommit Repository
+$Params = @{
+    PropertyObject    = @{
+        repoUrl = $builder.ToString()
+        branch = "master"
+        isManualIntegration = $true
+        deploymentRollbackEnabled = $false
+        isMercurial = $false
+    }
+    ResourceGroupName = $Settings.ResourceGroupName 
+    ResourceType      = 'Microsoft.Web/sites/SourceControls'
+    ResourceName      = '{0}/web' -f $Settings.ResourceGroupName
+    ApiVersion        = '2015-08-01'
+    force             = $true
+}
+$AzureAssets['WebAppDeploymentSettingsSet'] = Set-AzureRmResource @Params -ErrorAction 'SilentlyContinue'
+
+Describe "Azure Web App Deployment Settings" {
+    It "Was configured for CodeCommit" {
+        $Params = @{
+            ResourceGroupName = $Settings.ResourceGroupName 
+            ResourceType      = 'Microsoft.Web/sites/SourceControls'
+            ResourceName      = '{0}/web' -f $Settings.ResourceGroupName
+            ApiVersion        = '2015-08-01'
+        }
+        $AzureAssets['WebAppDeploymentSettingsGet'] = Get-AzureRmResource @Params
+        $Result = $AzureAssets.WebAppDeploymentSettingsGet
+
+        $Result.Properties.repoUrl | Should -BeExactly $builder.ToString()
+        $Result.Properties.branch | Should -BeExactly 'master'
+        $Result.Properties.isManualIntegration | Should -BeExactly $true
+        $Result.Properties.isMercurial | Should -BeExactly $false
+        $Result.Properties.deploymentRollbackEnabled | Should -BeExactly $false
+    }
+    It "Successfully provisioned from AWS CodeCommit" {
+        $AzureAssets['WebAppDeploymentSettingsGet'].Properties.provisioningState | Should -BeExactly 'Succeeded'
+    }
+}
+
+
+
+# Copy the Sample function app, commit the changes and push to AWS CodeCommit
 Copy-Item -Recurse ('{0}\*' -f $Settings.SrcDirectory) -Destination .
 git add -A
 git commit -m 'Add Example Function'
 git push origin master
-
-
 
 ##### Diagnostics and Cleanup
 $null = Start-AzureRmWebApp -WebApp $AzureAssets['WebbApps'][0]
 
 Remove-AzureRmResourceGroup -Name $Settings.ResourceGroupName -Force
 Remove-CCRepository -RepositoryName $Settings.CCRepoName -Force
+Remove-IAMUser -UserName $Settings.CCGitUser -Force
 Pop-Location; Pop-Location
 Remove-Item -force -Recurse -confirm:$false $Settings.GitDirectory
 Remove-Item -force -Recurse $Settings.ResourceGroupName
